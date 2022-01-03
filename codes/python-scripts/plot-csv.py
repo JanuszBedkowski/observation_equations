ae4c84f4488c3f9187c03620a03e55575ce2342c
Karol Majek <karolmajek@gmail.com>
2022-01-03 19:20:37 +0100

    plot csv

--- codes/python-scripts/plot-csv.py
+++ codes/python-scripts/plot-csv.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
-try:
-    import seaborn as sns
-    sns.set(style='whitegrid',font_scale=1.2)
-except:
-    print("Run:\nsudo pip3 install seaborn")
+# try:
+#     import seaborn as sns
+#     sns.set(style='whitegrid',font_scale=1.2)
+# except:
+#     print("Run:\nsudo pip3 install seaborn")
+
+
 
 
 fname='sample-data-to-plot.csv'
 delim=','
 SIZE=(14,10)
 
-def plot_csv(fname, output, title=None, ylabel='', delim=',', SIZE=(8,6)):
+def plot_csv(fname, output, title=None, ylabel='', delim=',', SIZE=(8,6), ymax=None):
     print(fname)
     with open(fname)as f:
         data = f.read().split('\n')
@@ -39,7 +41,12 @@ def plot_csv(fname, output, title=None, ylabel='', delim=',', SIZE=(8,6)):
         if title is not None:
             plt.title(title)
         plt.legend()
+
+        if ymax is not None:
+            plt.ylim(0,ymax)
+
         plt.grid(True)
+        # plt.axis('equal')
         plt.tight_layout()
         plt.savefig(output)
 
@@ -48,9 +55,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--title', default=None, help='plot title; default: no title')
     parser.add_argument('-d', '--delim', default=',', help='CSV delimiter, default: ,')
+    parser.add_argument('--ymax', default=None, help='ymax')
     parser.add_argument('-y', '--ylabel', default=None, help='Label for Y axis; default: no label')
     parser.add_argument('-i', '--input', help='CSV, comma-separated; delimiter can be changed using --delim',required=True)
     parser.add_argument('-o', '--output', help='Output file.eps',required=True)
     args = parser.parse_args()
-    plot_csv(args.input, args.output, title=args.title, ylabel=args.ylabel, delim=delim)
+    plot_csv(args.input, args.output, title=args.title, ylabel=args.ylabel, delim=delim, ymax=float(args.ymax))
 
