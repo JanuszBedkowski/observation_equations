9869cbe245fd18cc7c496ffe4701fbbc5b10cc6f
janusz bedkowski <januszbedkowski@gmail.com>
2022-01-23 18:20:46 +0100

    fix naming

--- codes/python-scripts/camera-metrics/perspective_camera_with_intrinsics_tait_bryan_cw_jacobian.py
+++ codes/python-scripts/camera-metrics/perspective_camera_with_intrinsics_tait_bryan_cw_jacobian.py
@@ -6,30 +6,30 @@ from rodrigues_R_utils import *
 from quaternion_R_utils import *
 
 fx,fy,cx,cy = symbols('fx fy cx cy');
-px, py, pz = symbols('px py pz')
+tx, ty, tz = symbols('tx ty tz')
 om, fi, ka = symbols('om fi ka')
 #sx, sy, sz = symbols('sx sy sz')
 #q0, q1, q2, q3 = symbols('q0 q1 q2 q3')
-tie_px, tie_py, tie_pz = symbols('tie_px tie_py tie_pz'); 
+pg_x, pg_y, pg_z = symbols('pg_x pg_y pg_z'); 
 u_kp, v_kp = symbols('u_kp v_kp')
 k1,k2,k3,p1,p2 = symbols("k1 k2 k3 p1 p2")
 
-position_symbols = [px, py, pz]
+position_symbols = [tx, ty, tz]
 orientation_symbols = [om, fi, ka]
 #orientation_symbols = [sx, sy, sz]
 #orientation_symbols = [q0, q1, q2, q3]
-tie_point_symbols = [tie_px, tie_py, tie_pz]
+point_global_symbols = [pg_x, pg_y, pg_z]
 intrinsics_symbols = [k1,k2,k3,p1,p2]
-all_symbols = intrinsics_symbols + position_symbols + orientation_symbols + tie_point_symbols
+all_symbols = intrinsics_symbols + position_symbols + orientation_symbols + point_global_symbols
 
-RT_cw = matrix44FromTaitBryan(px, py, pz, om, fi, ka)
-#RT_cw = matrix44FromRodrigues(px, py, pz, sx, sy, sz)
-#RT_cw = matrix44FromQuaternion(px, py, pz, q0, q1, q2, q3)
-R_cw=RT_cw[:-1,:-1]
-T_cw=Matrix([px, py, pz]).vec()
+Rt_cw = matrix44FromTaitBryan(tx, ty, tz, om, fi, ka)
+#Rt_cw = matrix44FromRodrigues(tx, ty, tz, sx, sy, sz)
+#Rt_cw = matrix44FromQuaternion(tx, ty, tz, q0, q1, q2, q3)
+R_cw=Rt_cw[:-1,:-1]
+t_cw=Matrix([tx, ty, tz]).vec()
 
-point_global=Matrix([tie_px, tie_py, tie_pz]).vec()
-point_local = R_cw * point_global + T_cw;
+point_global=Matrix([pg_x, pg_y, pg_z]).vec()
+point_local = R_cw * point_global + t_cw;
 s = 1.0 / point_local[2];
 
 u1 = s * point_local[0]
@@ -52,18 +52,18 @@ print(obs_eq)
 print(obs_eq_jacobian)
 
 with open("perspective_camera_with_intrinsics_tait_bryan_cw_jacobian.h",'w') as f_cpp:  
-    f_cpp.write("inline void observation_equation_perspective_camera_with_intrinsics_tait_bryan_cw(Eigen::Matrix<double, 2, 1> &delta, double fx, double fy, double cx, double cy, double px, double py, double pz, double om, double fi, double ka, double tie_px, double tie_py, double tie_pz, double u_kp, double v_kp, double k1, double k2, double k3, double p1, double p2)\n")
+    f_cpp.write("inline void observation_equation_perspective_camera_with_intrinsics_tait_bryan_cw(Eigen::Matrix<double, 2, 1> &delta, double fx, double fy, double cx, double cy, double tx, double ty, double tz, double om, double fi, double ka, double pg_x, double pg_y, double pg_z, double u_kp, double v_kp, double k1, double k2, double k3, double p1, double p2)\n")
     f_cpp.write("{")
     f_cpp.write("delta.coeffRef(0,0) = %s;\n"%(ccode(obs_eq[0,0])))
     f_cpp.write("delta.coeffRef(1,0) = %s;\n"%(ccode(obs_eq[1,0])))
     f_cpp.write("}")
-    f_cpp.write("inline void projection_perspective_camera_with_intrinsics_tait_bryan_cw(double &u, double &v, double fx, double fy, double cx, double cy, double px, double py, double pz, double om, double fi, double ka, double tie_px, double tie_py, double tie_pz, double k1, double k2, double k3, double p1, double p2)\n")
+    f_cpp.write("inline void projection_perspective_camera_with_intrinsics_tait_bryan_cw(double &u, double &v, double fx, double fy, double cx, double cy, double tx, double ty, double tz, double om, double fi, double ka, double pg_x, double pg_y, double pg_z, double k1, double k2, double k3, double p1, double p2)\n")
     f_cpp.write("{")
     f_cpp.write("u = %s;\n"%(ccode(uv[0,0])))
     f_cpp.write("v = %s;\n"%(ccode(uv[1,0])))
     f_cpp.write("}")
     f_cpp.write("\n")
-    f_cpp.write("inline void observation_equation_perspective_camera_with_intrinsics_tait_bryan_cw_jacobian(Eigen::Matrix<double, 2, 14> &j, double fx, double fy, double px, double py, double pz, double om, double fi, double ka, double tie_px, double tie_py, double tie_pz, double k1, double k2, double k3, double p1, double p2)\n")
+    f_cpp.write("inline void observation_equation_perspective_camera_with_intrinsics_tait_bryan_cw_jacobian(Eigen::Matrix<double, 2, 14> &j, double fx, double fy, double tx, double ty, double tz, double om, double fi, double ka, double pg_x, double pg_y, double pg_z, double k1, double k2, double k3, double p1, double p2)\n")
     f_cpp.write("{")
     for i in range (2):
         for j in range (14):
