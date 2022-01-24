fd5aec71e523f0beaa7c9eab0019fe096b948f70
janusz bedkowski <januszbedkowski@gmail.com>
2022-01-24 08:49:17 +0100

    naming convention

--- codes/python-scripts/metrics-fusion/ray_intersection_observation_equation_jacobian.h
+++ codes/python-scripts/metrics-fusion/ray_intersection_observation_equation_jacobian.h
@@ -1,8 +1,8 @@
-inline void ray_intersection_observation_equation(Eigen::Matrix<double, 2, 1> &delta, double T_x_int, double T_y_int, double T_z_int, double T_x_ln, double T_y_ln, double T_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)
-{delta.coeffRef(0,0) = -vxx*(T_x_int - T_x_ln) - vxy*(T_y_int - T_y_ln) - vxz*(T_z_int - T_z_ln);
-delta.coeffRef(1,0) = -vyx*(T_x_int - T_x_ln) - vyy*(T_y_int - T_y_ln) - vyz*(T_z_int - T_z_ln);
+inline void ray_intersection_observation_equation(Eigen::Matrix<double, 2, 1> &delta, double t_x_int, double t_y_int, double t_z_int, double t_x_ln, double t_y_ln, double t_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)
+{delta.coeffRef(0,0) = -vxx*(t_x_int - t_x_ln) - vxy*(t_y_int - t_y_ln) - vxz*(t_z_int - t_z_ln);
+delta.coeffRef(1,0) = -vyx*(t_x_int - t_x_ln) - vyy*(t_y_int - t_y_ln) - vyz*(t_z_int - t_z_ln);
 }
-inline void ray_intersection_observation_equation_jacobian(Eigen::Matrix<double, 2, 3> &j, double T_x_int, double T_y_int, double T_z_int, double T_x_ln, double T_y_ln, double T_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)
+inline void ray_intersection_observation_equation_jacobian(Eigen::Matrix<double, 2, 3> &j, double t_x_int, double t_y_int, double t_z_int, double t_x_ln, double t_y_ln, double t_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)
 {j.coeffRef(0,0) = -vxx;
 j.coeffRef(0,1) = -vxy;
 j.coeffRef(0,2) = -vxz;
--- codes/python-scripts/metrics-fusion/ray_intersection_observation_equation_jacobian.py
+++ codes/python-scripts/metrics-fusion/ray_intersection_observation_equation_jacobian.py
@@ -1,20 +1,20 @@
 from sympy import *
 
-T_x_int, T_y_int, T_z_int = symbols('T_x_int T_y_int T_z_int')
-T_x_ln, T_y_ln, T_z_ln = symbols('T_x_ln T_y_ln T_z_ln')
+t_x_int, t_y_int, t_z_int = symbols('t_x_int t_y_int t_z_int')
+t_x_ln, t_y_ln, t_z_ln = symbols('t_x_ln t_y_ln t_z_ln')
 vxx, vxy, vxz = symbols('vxx vxy vxz')
 vyx, vyy, vyz = symbols('vyx vyy vyz')
 
-ray_intersection_symbols = [T_x_int, T_y_int, T_z_int]
+ray_intersection_symbols = [t_x_int, t_y_int, t_z_int]
 all_symbols = ray_intersection_symbols
 
-T_int = Matrix([T_x_int, T_y_int, T_z_int])
-T_ln = Matrix([T_x_ln, T_y_ln, T_z_ln])
+t_int = Matrix([t_x_int, t_y_int, t_z_int])
+t_ln = Matrix([t_x_ln, t_y_ln, t_z_ln])
 vxt = Matrix([[vxx, vxy, vxz]]).transpose()
 vyt = Matrix([[vyx, vyy, vyz]]).transpose()
 
 target_value = Matrix([0,0]).vec()
-model_function = Matrix([vxt.dot(T_int - T_ln),vyt.dot(T_int - T_ln)]).vec()
+model_function = Matrix([vxt.dot(t_int - t_ln),vyt.dot(t_int - t_ln)]).vec()
 delta = target_value - model_function
 delta_jacobian=delta.jacobian(all_symbols)
 
@@ -22,13 +22,13 @@ print(delta)
 print(delta_jacobian)
 
 with open("ray_intersection_observation_equation_jacobian.h",'w') as f_cpp:  
-    f_cpp.write("inline void ray_intersection_observation_equation(Eigen::Matrix<double, 2, 1> &delta, double T_x_int, double T_y_int, double T_z_int, double T_x_ln, double T_y_ln, double T_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)\n")
+    f_cpp.write("inline void ray_intersection_observation_equation(Eigen::Matrix<double, 2, 1> &delta, double t_x_int, double t_y_int, double t_z_int, double t_x_ln, double t_y_ln, double t_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)\n")
     f_cpp.write("{")
     f_cpp.write("delta.coeffRef(0,0) = %s;\n"%(ccode(delta[0,0])))
     f_cpp.write("delta.coeffRef(1,0) = %s;\n"%(ccode(delta[1,0])))
     f_cpp.write("}")
     f_cpp.write("\n")
-    f_cpp.write("inline void ray_intersection_observation_equation_jacobian(Eigen::Matrix<double, 2, 3> &j, double T_x_int, double T_y_int, double T_z_int, double T_x_ln, double T_y_ln, double T_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)\n")
+    f_cpp.write("inline void ray_intersection_observation_equation_jacobian(Eigen::Matrix<double, 2, 3> &j, double t_x_int, double t_y_int, double t_z_int, double t_x_ln, double t_y_ln, double t_z_ln, double vxx, double vxy, double vxz, double vyx, double vyy, double vyz)\n")
     f_cpp.write("{")
     for i in range (2):
         for j in range (3):
