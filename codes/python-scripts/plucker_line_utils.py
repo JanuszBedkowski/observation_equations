1b6589a096991142433a6c3fe4446fcc405d1436
janusz bedkowski <januszbedkowski@gmail.com>
2022-01-24 18:37:44 +0100

    naming

--- codes/python-scripts/plucker_line_utils.py
+++ codes/python-scripts/plucker_line_utils.py
@@ -3,13 +3,13 @@ from sympy import *
 def plucker_line_K(fx, fy, cx, cy):
     return Matrix([[fy,0,0],[0,fx,0],[-fy*cx,-fx*cy,fx*fy]])
 
-def plucker_line_motion_matrix_cw(RT_wc):
-    a=RT_wc[0,3]
-    b=RT_wc[1,3]
-    c=RT_wc[2,3]
-    rt=Matrix([[RT_wc[0,0], RT_wc[1,0], RT_wc[2,0]],
-               [RT_wc[0,1], RT_wc[1,1], RT_wc[2,1]],
-               [RT_wc[0,2], RT_wc[1,2], RT_wc[2,2]]])
+def plucker_line_motion_matrix_cw(Rt_wc):
+    a=Rt_wc[0,3]
+    b=Rt_wc[1,3]
+    c=Rt_wc[2,3]
+    rt=Matrix([[Rt_wc[0,0], Rt_wc[1,0], Rt_wc[2,0]],
+               [Rt_wc[0,1], Rt_wc[1,1], Rt_wc[2,1]],
+               [Rt_wc[0,2], Rt_wc[1,2], Rt_wc[2,2]]])
     tx=Matrix([[0,-c,b],[c,0,-a],[-b,a,0]])
     rttx=rt*tx
     return Matrix([[rt[0,0], rt[0,1], rt[0,2], -rttx[0,0], -rttx[0,1], -rttx[0,2]],
@@ -19,13 +19,13 @@ def plucker_line_motion_matrix_cw(RT_wc):
                    [0,0,0, rt[1,0], rt[1,1], rt[1,2]],
                    [0,0,0, rt[2,0], rt[2,1], rt[2,2]]])
 
-def plucker_line_motion_matrix_wc(RT_wc):
-    a=RT_wc[0,3]
-    b=RT_wc[1,3]
-    c=RT_wc[2,3]
-    r=Matrix([[RT_wc[0,0], RT_wc[0,1], RT_wc[0,2]],
-              [RT_wc[1,0], RT_wc[1,1], RT_wc[1,2]],
-              [RT_wc[2,0], RT_wc[2,1], RT_wc[2,2]]])
+def plucker_line_motion_matrix_wc(Rt_wc):
+    a=Rt_wc[0,3]
+    b=Rt_wc[1,3]
+    c=Rt_wc[2,3]
+    r=Matrix([[Rt_wc[0,0], Rt_wc[0,1], Rt_wc[0,2]],
+              [Rt_wc[1,0], Rt_wc[1,1], Rt_wc[1,2]],
+              [Rt_wc[2,0], Rt_wc[2,1], Rt_wc[2,2]]])
     tx=Matrix([[0,-c,b],[c,0,-a],[-b,a,0]])
     txr=tx*r
     return Matrix([[r[0,0], r[0,1], r[0,2], txr[0,0], txr[0,1], txr[0,2]],
