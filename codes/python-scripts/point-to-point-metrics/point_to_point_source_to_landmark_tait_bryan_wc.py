0a643adf703d5c14c7ec65c43646657300f4c4a9
janusz bedkowski <januszbedkowski@gmail.com>
2022-01-26 09:00:33 +0100

    naming

--- codes/python-scripts/point-to-point-metrics/point_to_point_source_to_landmark_tait_bryan_wc.py
+++ codes/python-scripts/point-to-point-metrics/point_to_point_source_to_landmark_tait_bryan_wc.py
@@ -7,12 +7,12 @@ from quaternion_R_utils import *
 
 x_L, y_L, z_L = symbols('x_L y_L z_L')
 x_s, y_s, z_s = symbols('x_s y_s z_s')
-px, py, pz = symbols('px py pz')
+tx, ty, tz = symbols('tx ty tz')
 om, fi, ka = symbols('om fi ka')
 #sx, sy, sz = symbols('sx sy sz')
 #q0, q1, q2, q3 = symbols('q0 q1 q2 q3')
 
-position_symbols = [px, py, pz]
+position_symbols = [tx, ty, tz]
 orientation_symbols = [om, fi, ka]
 #orientation_symbols = [sx, sy, sz]
 #orientation_symbols = [q0, q1, q2, q3]
@@ -22,9 +22,9 @@ all_symbols = position_symbols + orientation_symbols + landmark_symbols
 
 point_Landmark = Matrix([x_L, y_L, z_L]).vec()
 point_source = Matrix([x_s, y_s, z_s, 1]).vec()
-transformed_point_source = (matrix44FromTaitBryan(px, py, pz, om, fi, ka) * point_source)[:-1,:]
-#transformed_point_s = (matrix44FromRodrigues(px, py, pz, sx, sy, sz) * point_source)[:-1,:]
-#transformed_point_s = (matrix44FromQuaternion(px, py, pz, q0, q1, q2, q3) * point_source)[:-1,:]
+transformed_point_source = (matrix44FromTaitBryan(tx, ty, tz, om, fi, ka) * point_source)[:-1,:]
+#transformed_point_s = (matrix44FromRodrigues(tx, ty, tz, sx, sy, sz) * point_source)[:-1,:]
+#transformed_point_s = (matrix44FromQuaternion(tx, ty, tz, q0, q1, q2, q3) * point_source)[:-1,:]
 
 target_value = Matrix([0,0,0]).vec()
 model_function = transformed_point_source-point_Landmark
@@ -35,14 +35,14 @@ print(delta)
 print(delta_jacobian)
 
 with open("point_to_point_source_to_landmark_tait_bryan_wc_jacobian.h",'w') as f_cpp:  
-    f_cpp.write("inline void point_to_point_source_to_landmark_tait_bryan_wc(double &delta_x, double &delta_y, double &delta_z, double px, double py, double pz, double om, double fi, double ka, double x_s, double y_s, double z_s, double x_L, double y_L, double z_L)\n")
+    f_cpp.write("inline void point_to_point_source_to_landmark_tait_bryan_wc(double &delta_x, double &delta_y, double &delta_z, double tx, double ty, double tz, double om, double fi, double ka, double x_s, double y_s, double z_s, double x_L, double y_L, double z_L)\n")
     f_cpp.write("{")
     f_cpp.write("delta_x = %s;\n"%(ccode(delta[0,0])))
     f_cpp.write("delta_y = %s;\n"%(ccode(delta[1,0])))
     f_cpp.write("delta_z = %s;\n"%(ccode(delta[2,0])))
     f_cpp.write("}")
     f_cpp.write("\n")
-    f_cpp.write("inline void point_to_point_source_to_landmark_tait_bryan_wc_jacobian(Eigen::Matrix<double, 3, 9, Eigen::RowMajor> &j, double px, double py, double pz, double om, double fi, double ka, double x_s, double y_s, double z_s)\n")
+    f_cpp.write("inline void point_to_point_source_to_landmark_tait_bryan_wc_jacobian(Eigen::Matrix<double, 3, 9, Eigen::RowMajor> &j, double tx, double ty, double tz, double om, double fi, double ka, double x_s, double y_s, double z_s)\n")
     f_cpp.write("{")
     for i in range (3):
         for j in range (9):
--- codes/python-scripts/point-to-point-metrics/point_to_point_source_to_landmark_tait_bryan_wc_jacobian.h
+++ codes/python-scripts/point-to-point-metrics/point_to_point_source_to_landmark_tait_bryan_wc_jacobian.h
@@ -1,9 +1,9 @@
-inline void point_to_point_source_to_landmark_tait_bryan_wc(double &delta_x, double &delta_y, double &delta_z, double px, double py, double pz, double om, double fi, double ka, double x_s, double y_s, double z_s, double x_L, double y_L, double z_L)
-{delta_x = -px + x_L - x_s*cos(fi)*cos(ka) + y_s*sin(ka)*cos(fi) - z_s*sin(fi);
-delta_y = -py - x_s*(sin(fi)*sin(om)*cos(ka) + sin(ka)*cos(om)) + y_L - y_s*(-sin(fi)*sin(ka)*sin(om) + cos(ka)*cos(om)) + z_s*sin(om)*cos(fi);
-delta_z = -pz - x_s*(-sin(fi)*cos(ka)*cos(om) + sin(ka)*sin(om)) - y_s*(sin(fi)*sin(ka)*cos(om) + sin(om)*cos(ka)) + z_L - z_s*cos(fi)*cos(om);
+inline void point_to_point_source_to_landmark_tait_bryan_wc(double &delta_x, double &delta_y, double &delta_z, double tx, double ty, double tz, double om, double fi, double ka, double x_s, double y_s, double z_s, double x_L, double y_L, double z_L)
+{delta_x = -tx + x_L - x_s*cos(fi)*cos(ka) + y_s*sin(ka)*cos(fi) - z_s*sin(fi);
+delta_y = -ty - x_s*(sin(fi)*sin(om)*cos(ka) + sin(ka)*cos(om)) + y_L - y_s*(-sin(fi)*sin(ka)*sin(om) + cos(ka)*cos(om)) + z_s*sin(om)*cos(fi);
+delta_z = -tz - x_s*(-sin(fi)*cos(ka)*cos(om) + sin(ka)*sin(om)) - y_s*(sin(fi)*sin(ka)*cos(om) + sin(om)*cos(ka)) + z_L - z_s*cos(fi)*cos(om);
 }
-inline void point_to_point_source_to_landmark_tait_bryan_wc_jacobian(Eigen::Matrix<double, 3, 9, Eigen::RowMajor> &j, double px, double py, double pz, double om, double fi, double ka, double x_s, double y_s, double z_s)
+inline void point_to_point_source_to_landmark_tait_bryan_wc_jacobian(Eigen::Matrix<double, 3, 9, Eigen::RowMajor> &j, double tx, double ty, double tz, double om, double fi, double ka, double x_s, double y_s, double z_s)
 {j.coeffRef(0,0) = -1;
 j.coeffRef(0,1) = 0;
 j.coeffRef(0,2) = 0;
