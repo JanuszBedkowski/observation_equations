36ef60a4ef7f0e4e7924c256ac3a70c91e388e5a
janusz bedkowski <januszbedkowski@gmail.com>
2022-01-26 09:24:20 +0100

    naming

--- codes/python-scripts/point-to-feature-metrics/distance_point_to_plane_tait_bryan_wc.py
+++ codes/python-scripts/point-to-feature-metrics/distance_point_to_plane_tait_bryan_wc.py
@@ -7,22 +7,22 @@ from quaternion_R_utils import *
 
 x, y, z = symbols('x y z')
 a, b, c, d = symbols('a b c d')
-px, py, pz = symbols('px py pz')
+tx, ty, tz = symbols('tx ty tz')
 om, fi, ka = symbols('om fi ka')
 #sx, sy, sz = symbols('sx sy sz')
 #q0, q1, q2, q3 = symbols('q0 q1 q2 q3')
 
-position_symbols = [px, py, pz]
+position_symbols = [tx, ty, tz]
 orientation_symbols = [om, fi, ka]
 #orientation_symbols = [sx, sy, sz]
 #orientation_symbols = [q0, q1, q2, q3]
 all_symbols = position_symbols + orientation_symbols
 
 point_source_local = Matrix([x, y, z, 1]).vec()
-RT_wc = matrix44FromTaitBryan(px, py, pz, om, fi, ka)[:-1,:]
-#RT_wc = matrix44FromRodrigues(px, py, pz, sx, sy, sz)[:-1,:]
-#RT_wc = matrix44FromQuaternion(px, py, pz, q0, q1, q2, q3)[:-1,:]
-point_source_global = RT_wc * point_source_local
+Rt_wc = matrix44FromTaitBryan(tx, ty, tz, om, fi, ka)[:-1,:]
+#Rt_wc = matrix44FromRodrigues(tx, ty, tz, sx, sy, sz)[:-1,:]
+#Rt_wc = matrix44FromQuaternion(tx, ty, tz, q0, q1, q2, q3)[:-1,:]
+point_source_global = Rt_wc * point_source_local
 
 target_value = Matrix([0]).vec()
 model_function = Matrix([a*point_source_global[0] + b * point_source_global[1] + c * point_source_global[2] + d]).vec()
@@ -33,12 +33,12 @@ print(delta)
 print(delta_jacobian)
 
 with open("distance_point_to_plane_tait_bryan_wc_jacobian.h",'w') as f_cpp:  
-    f_cpp.write("inline void delta_distance_point_to_plane_tait_bryan_wc(Eigen::Matrix<double, 1, 1> &delta, double px, double py, double pz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)\n")
+    f_cpp.write("inline void delta_distance_point_to_plane_tait_bryan_wc(Eigen::Matrix<double, 1, 1> &delta, double tx, double ty, double tz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)\n")
     f_cpp.write("{")
     f_cpp.write("delta.coeffRef(0,0) = %s;\n"%(ccode(delta[0,0])))
     f_cpp.write("}")
     f_cpp.write("\n")
-    f_cpp.write("inline void delta_distance_point_to_plane_tait_bryan_wc_jacobian(Eigen::Matrix<double, 1, 6> &j, double px, double py, double pz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)\n")
+    f_cpp.write("inline void delta_distance_point_to_plane_tait_bryan_wc_jacobian(Eigen::Matrix<double, 1, 6> &j, double tx, double ty, double tz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)\n")
     f_cpp.write("{")
     for i in range (1):
         for j in range (6):
--- codes/python-scripts/point-to-feature-metrics/distance_point_to_plane_tait_bryan_wc_jacobian.h
+++ codes/python-scripts/point-to-feature-metrics/distance_point_to_plane_tait_bryan_wc_jacobian.h
@@ -1,7 +1,7 @@
-inline void delta_distance_point_to_plane_tait_bryan_wc(Eigen::Matrix<double, 1, 1> &delta, double px, double py, double pz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)
-{delta.coeffRef(0,0) = -a*(px + x*cos(fi)*cos(ka) - y*sin(ka)*cos(fi) + z*sin(fi)) - b*(py + x*(sin(fi)*sin(om)*cos(ka) + sin(ka)*cos(om)) + y*(-sin(fi)*sin(ka)*sin(om) + cos(ka)*cos(om)) - z*sin(om)*cos(fi)) - c*(pz + x*(-sin(fi)*cos(ka)*cos(om) + sin(ka)*sin(om)) + y*(sin(fi)*sin(ka)*cos(om) + sin(om)*cos(ka)) + z*cos(fi)*cos(om)) - d;
+inline void delta_distance_point_to_plane_tait_bryan_wc(Eigen::Matrix<double, 1, 1> &delta, double tx, double ty, double tz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)
+{delta.coeffRef(0,0) = -a*(tx + x*cos(fi)*cos(ka) - y*sin(ka)*cos(fi) + z*sin(fi)) - b*(ty + x*(sin(fi)*sin(om)*cos(ka) + sin(ka)*cos(om)) + y*(-sin(fi)*sin(ka)*sin(om) + cos(ka)*cos(om)) - z*sin(om)*cos(fi)) - c*(tz + x*(-sin(fi)*cos(ka)*cos(om) + sin(ka)*sin(om)) + y*(sin(fi)*sin(ka)*cos(om) + sin(om)*cos(ka)) + z*cos(fi)*cos(om)) - d;
 }
-inline void delta_distance_point_to_plane_tait_bryan_wc_jacobian(Eigen::Matrix<double, 1, 6> &j, double px, double py, double pz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)
+inline void delta_distance_point_to_plane_tait_bryan_wc_jacobian(Eigen::Matrix<double, 1, 6> &j, double tx, double ty, double tz, double om, double fi, double ka, double x, double y, double z, double a, double b, double c, double d)
 {j.coeffRef(0,0) = -a;
 j.coeffRef(0,1) = -b;
 j.coeffRef(0,2) = -c;
